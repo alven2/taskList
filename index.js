d4c7f573e6b2086157f6e594e688c6855faefbd4
alven2 <alvenqiuzhang2@gmail.com>
2025-06-28 01:56:07 +0000

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('hola')
+function addTask() {
+    
+    const $taskInput = document.querySelector('.taskInput');
+    const $taskText = $taskInput.value.trim();
+    
+    if ($taskText !== '') {
+        
+    const $taskList = document.querySelector('.taskList');
+    const li = document.createElement('li');
+    li.textContent = $taskText;
+    
+    li.onclick = function() {
+        if (li.style.textDecoration === 'line-through') {
+            li.style.textDecoration = 'none'
+        } else {
+            li.style.textDecoration = 'line-through'
+        }
+    }
+    
+    const erase = document.createElement('button');
+            erase.textContent = 'x';
+    erase.onclick = function() {
+            $taskList.removeChild(li)
+    }
+   
+    // despliege //
+    $taskList.appendChild(li)
+    $taskInput.value = '';
+    li.appendChild(erase);
+    }
